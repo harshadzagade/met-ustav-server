0e22a0c49487e591dc069c4ee2f873a2b70b2596
Harshad Zagade <Hzagade21@gmail.com>
2024-12-15 10:51:22 +0530

    Update controller update

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,24 +1,50 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const { check, validationResult } = require('express-validator');
-const User = require('../models/User');
-const Institute = require('../models/Institute');
-const Category = require('../models/Category');
-require('dotenv').config();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { check, validationResult } = require("express-validator");
+const User = require("../models/User");
+const Institute = require("../models/Institute");
+const Category = require("../models/Category");
+require("dotenv").config();
 
 exports.registerUser = [
-  check('firstName').isLength({ min: 3, max: 50 }).withMessage('First name must be between 3 and 50 characters'),
-  check('middleName').isLength({ min: 3, max: 50 }).withMessage('Middle name must be between 3 and 50 characters'),
-  check('lastName').isLength({ min: 3, max: 50 }).withMessage('Last name must be between 3 and 50 characters'),
-  check('gender').isIn(['Male', 'Female', 'Other']).withMessage('Invalid gender'),
-  check('email').isEmail().withMessage('Invalid email'),
-  check('password').isLength({ min: 4, max: 4 }).withMessage('Password must be between 8 and 128 characters'),
-  check('role').isIn(['HOE', 'Student', 'Staff', 'Coordinator', 'Volunteer', 'Trustee', 'User']).withMessage('Invalid role'),
-  check('type').isIn(['Student', 'Staff']).withMessage('Invalid type. Please select Student or Staff'),
-  check('rollNo').isLength({ min: 1, max: 10 }).withMessage('Roll number must be between 1 and 10 characters'),
-  check('instituteId').isInt().withMessage('Invalid institute ID'),
-  check('phoneNo').isLength({ min: 10, max: 10 }).withMessage('Phone number must be 10 characters'),
-  check('categoryId').isInt().withMessage('Invalid category ID'),
+  check("firstName")
+    .isLength({ min: 3, max: 50 })
+    .withMessage("First name must be between 3 and 50 characters"),
+  check("middleName")
+    .isLength({ min: 3, max: 50 })
+    .withMessage("Middle name must be between 3 and 50 characters"),
+  check("lastName")
+    .isLength({ min: 3, max: 50 })
+    .withMessage("Last name must be between 3 and 50 characters"),
+  check("gender")
+    .isIn(["Male", "Female", "Other"])
+    .withMessage("Invalid gender"),
+  check("email").isEmail().withMessage("Invalid email"),
+  check("password")
+    .isLength({ min: 4, max: 4 })
+    .withMessage("Password must be between 8 and 128 characters"),
+  check("role")
+    .isIn([
+      "HOE",
+      "Student",
+      "Staff",
+      "Coordinator",
+      "Volunteer",
+      "Trustee",
+      "User",
+    ])
+    .withMessage("Invalid role"),
+  check("type")
+    .isIn(["Student", "Staff"])
+    .withMessage("Invalid type. Please select Student or Staff"),
+  check("rollNo")
+    .isLength({ min: 1, max: 10 })
+    .withMessage("Roll number must be between 1 and 10 characters"),
+  check("instituteId").isInt().withMessage("Invalid institute ID"),
+  check("phoneNo")
+    .isLength({ min: 10, max: 10 })
+    .withMessage("Phone number must be 10 characters"),
+  check("categoryId").isInt().withMessage("Invalid category ID"),
 
   async (req, res) => {
     const errors = validationResult(req);
@@ -26,19 +52,19 @@ exports.registerUser = [
       return res.status(422).json({ errors: errors.array() });
     }
 
-    const { 
-      firstName, 
-      middleName, 
-      lastName, 
-      gender, 
-      email, 
-      password, 
-      role, 
+    const {
+      firstName,
+      middleName,
+      lastName,
+      gender,
+      email,
+      password,
+      role,
       type,
-      rollNo, 
-      instituteId, 
-      phoneNo, 
-      categoryId 
+      rollNo,
+      instituteId,
+      phoneNo,
+      categoryId,
     } = req.body;
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -46,7 +72,7 @@ exports.registerUser = [
     try {
       const existingUser = await User.findOne({ where: { email } });
       if (existingUser) {
-        return res.status(400).json({ msg: 'User already exists' });
+        return res.status(400).json({ msg: "User already exists" });
       }
 
       const newUser = await User.create({
@@ -67,14 +93,16 @@ exports.registerUser = [
       res.json(newUser);
     } catch (error) {
       console.error(error);
-      res.status(500).json({ msg: 'Error creating user' });
+      res.status(500).json({ msg: "Error creating user" });
     }
   },
 ];
 
 exports.loginUser = [
-  check('email').isEmail().withMessage('Invalid email'),
-  check('password').isLength({ min: 4, max: 4 }).withMessage('Password must be between 4 and 4 characters'),
+  check("email").isEmail().withMessage("Invalid email"),
+  check("password")
+    .isLength({ min: 4, max: 4 })
+    .withMessage("Password must be between 4 and 4 characters"),
 
   async (req, res) => {
     const errors = validationResult(req);
@@ -87,22 +115,22 @@ exports.loginUser = [
     try {
       const user = await User.findOne({ where: { email } });
       if (!user) {
-        return res.status(404).json({ msg: 'User not found' });
+        return res.status(404).json({ msg: "User not found" });
       }
 
       const isValidPassword = await bcrypt.compare(password, user.password);
       if (!isValidPassword) {
-        return res.status(401).json({ msg: 'Invalid password' });
+        return res.status(401).json({ msg: "Invalid password" });
       }
 
       const token = jwt.sign({ userId: user.id }, process.env.SECRET_KEY, {
-        expiresIn: '1h',
+        expiresIn: "1h",
       });
 
       res.json({ token, user });
     } catch (error) {
       console.error(error);
-      res.status(500).json({ msg: 'Error logging in user' });
+      res.status(500).json({ msg: "Error logging in user" });
     }
   },
 ];
@@ -110,43 +138,71 @@ exports.loginUser = [
 exports.getAllUsers = async (req, res) => {
   try {
     const users = await User.findAll({
-      include: [{ model: Category, as: 'Category' }, { model: Institute, as: 'Institute' }], 
+      include: [
+        { model: Category, as: "Category" },
+        { model: Institute, as: "Institute" },
+      ],
     });
     res.json(users);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ msg: 'Error getting users' });
+    res.status(500).json({ msg: "Error getting users" });
   }
 };
 
-
 exports.getUser = async (req, res) => {
   try {
-    const user = await User.findByPk(req.params.id);  
+    const user = await User.findByPk(req.params.id);
     if (!user) {
-      return res.status(404).json({ msg: 'User not found' });
+      return res.status(404).json({ msg: "User not found" });
     }
     res.json(user);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ msg: 'Error getting user' });
+    res.status(500).json({ msg: "Error getting user" });
   }
 };
 
 exports.updateUser = [
-  check('name').optional().isLength({ min: 3, max: 50 }).withMessage('Name must be between 3 and 50 characters'),
-  check('gender').optional().isIn(['Male', 'Female', 'Other']).withMessage('Invalid gender'),
-  check('email').optional().isEmail().withMessage('Invalid email'),
-  check('password').optional().isLength({ min: 8, max: 128 }).withMessage('Password must be between 8 and 128 characters'),
-  check('role')
+  check("name")
     .optional()
-    .isIn(['HOE', 'Student', 'Staff', 'Coordinator', 'Volunteer', 'Trustee', 'User'])
-    .withMessage('Invalid role'),
-  check('type').optional().isIn(['Student', 'Staff']).withMessage('Invalid type'),
-  check('rollNo').optional().isLength({ min: 1, max: 10 }).withMessage('Roll number must be between 1 and 10 characters'),
-  check('phoneNo').optional().isLength({ min: 10, max: 10 }).withMessage('Phone number must be 10 characters'),
-  check('categoryId').optional().isInt().withMessage('Invalid category ID'),
-  check('instituteId').optional().isInt().withMessage('Invalid institute ID'),
+    .isLength({ min: 3, max: 50 })
+    .withMessage("Name must be between 3 and 50 characters"),
+  check("gender")
+    .optional()
+    .isIn(["Male", "Female", "Other"])
+    .withMessage("Invalid gender"),
+  check("email").optional().isEmail().withMessage("Invalid email"),
+  check("password")
+    .optional()
+    .isLength({ min: 8, max: 128 })
+    .withMessage("Password must be between 8 and 128 characters"),
+  check("role")
+    .optional({ nullable: true, checkFalsy: true })
+    .isIn([
+      "HOE",
+      "Student",
+      "Staff",
+      "Coordinator",
+      "Volunteer",
+      "Trustee",
+      "User",
+    ])
+    .withMessage("Invalid role"),
+  check("type")
+    .optional()
+    .isIn(["Student", "Staff"])
+    .withMessage("Invalid type"),
+  check("rollNo")
+    .optional()
+    .isLength({ min: 1, max: 10 })
+    .withMessage("Roll number must be between 1 and 10 characters"),
+  check("phoneNo")
+    .optional()
+    .isLength({ min: 10, max: 10 })
+    .withMessage("Phone number must be 10 characters"),
+  check("categoryId").optional().isInt().withMessage("Invalid category ID"),
+  check("instituteId").optional().isInt().withMessage("Invalid institute ID"),
 
   async (req, res) => {
     const errors = validationResult(req);
@@ -157,7 +213,7 @@ exports.updateUser = [
     try {
       const user = await User.findByPk(req.params.id);
       if (!user) {
-        return res.status(404).json({ msg: 'User not found' });
+        return res.status(404).json({ msg: "User not found" });
       }
 
       const updateData = { ...req.body };
@@ -169,29 +225,30 @@ exports.updateUser = [
 
       // Update user data dynamically with the keys in `req.body`
       Object.entries(updateData).forEach(([key, value]) => {
-        user[key] = value;
+        if (value !== "") {
+          user[key] = value;
+        }
       });
 
       await user.save();
       res.json(user);
     } catch (error) {
       console.error(error);
-      res.status(500).json({ msg: 'Error updating user' });
+      res.status(500).json({ msg: "Error updating user" });
     }
   },
 ];
-
 
 exports.deleteUser = async (req, res) => {
   try {
     const user = await User.findByPk(req.params.id);
     if (!user) {
-      return res.status(404).json({ msg: 'User not found' });
+      return res.status(404).json({ msg: "User not found" });
     }
     await user.destroy();
-    res.json({ msg: 'User deleted successfully' });
+    res.json({ msg: "User deleted successfully" });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ msg: 'Error deleting user' });
+    res.status(500).json({ msg: "Error deleting user" });
   }
 };
